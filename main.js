7775ce6be40e1d303f7063e8a929b7a5b0fa71f8
DuguidDa537 <DuguidDa537@northwested.org>
2026-03-09 11:19:47 -0400

    Add variable declarations and comments in main.js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+ // Danyl Fitzgerald Duguid
+    // March of 9th, 2026
+    // Declaring/Assigning JS Variables
+
+    const firstName = "Danyl";
+    const lastName = "Duguid";
+    const eyeColor = "Brown/Green";
+    const sendingSchool = "Elk Rapids";
+    const ctcProgram = "W&AD";
+    const grade = "11";
+
+    console.log(firstName);
+    console.log(
 
