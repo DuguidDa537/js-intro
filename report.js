96adc8c39c12e5676f5d9d33c72685c08516d1ae
DuguidDa537 <DuguidDa537@northwested.org>
2026-03-10 11:04:06 -0400

    Update report.js

--- report.js
+++ report.js
@@ -1,1 +1,16 @@
+//Danyl Duguid
+//March 10
+//grade report project
+const lastName="McDonald"
+const firstName="Ronald"
+const age="45"
+const quiz1="25"
+const quiz2="50"
+const quiz3="100"
 
+console.log(lastName)
+console.log(firstName)
+console.log(age)
+console.log(quiz1)
+console.log(quiz2)
+console.log(quiz3)
