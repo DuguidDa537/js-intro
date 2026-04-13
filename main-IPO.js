56d988e277f0fbeb68c2cadc0cfb7827f4260770
DuguidDa537 <DuguidDa537@northwested.org>
2026-04-13 10:18:39 -0400

    Create main-IPO.js

---
+++ main-IPO.js
@@ -0,0 +1,25 @@
+//  Danyl Duguid, Sir Bryce Perron of Elk Rapids, Antrim Country, 49629.
+
+//  4/13/1996
+
+//  Input Process Output model
+
+
+
+// input
+const quiz1 = 85.00;
+const quiz2 = 77.00;
+const quiz3 = 91.00;
+
+
+// process
+const average = (quiz1 + quiz2 + quiz3) / 3;
+
+
+//output
+console.log(`First quiz score: ${quiz1}`);
+console.log(`Second quiz score: ${quiz2}`);
+console.log(`Third quiz score: ${quiz3}`);
+console.log(`Average score: ${average.toFixed(2)}`);
+console.log(`预定死亡：现在`)
+console.log('Live reload enabled.')
